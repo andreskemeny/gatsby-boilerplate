446b36aa4e150aeb67163b6d7106eef5881085c1
Andres Kemeny <andres@kemeny.cl>
2021-02-10 14:54:55 -0300

    update readme, add SEO usage example in example subpage

--- src/components/PageExample/SubPageExample.tsx
+++ src/components/PageExample/SubPageExample.tsx
@@ -1,8 +1,14 @@
 import React from "react";
+import SEO from "../SEO";
 
 const Page = () => {
   return (
     <React.Fragment>
+      <SEO
+        title="Page title | Example Site"
+        description="Page description"
+        image={null} // Image null to keep the default site image (logo.svg)
+      />
       <p>Sub Page</p>
     </React.Fragment>
   );
